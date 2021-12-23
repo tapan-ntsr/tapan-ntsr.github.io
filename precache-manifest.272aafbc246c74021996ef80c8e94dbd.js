404513667c28cd7c67835dfc38885e4b34f17abd
Tapan Siddarth Narra <ntsr2003@gmail.com>
2021-12-23 11:03:07 +0530

    Updates

---
+++ precache-manifest.272aafbc246c74021996ef80c8e94dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1e4cc07a087f219b819",
+    "url": "/website/static/css/main.74d09049.chunk.css"
+  },
+  {
+    "revision": "c1e4cc07a087f219b819",
+    "url": "/website/static/js/main.c1e4cc07.chunk.js"
+  },
+  {
+    "revision": "41f96ad9b12a68fb853d",
+    "url": "/website/static/js/1.41f96ad9.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "c21794518c3e8e8a480a78473f597014",
+    "url": "/website/static/media/robot.c2179451.png"
+  },
+  {
+    "revision": "e9dd77dc47c21d8d9ec7a148c1741ce9",
+    "url": "/website/index.html"
+  }
+];
