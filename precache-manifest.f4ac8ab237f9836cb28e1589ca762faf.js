c8ae112fb93797e3601f21f2889a31b2a48be844
Tapan Siddarth Narra <ntsr2003@gmail.com>
2021-12-23 12:41:54 +0530

    Updates

---
+++ precache-manifest.f4ac8ab237f9836cb28e1589ca762faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1e4cc07a087f219b819",
+    "url": "/static/css/main.74d09049.chunk.css"
+  },
+  {
+    "revision": "c1e4cc07a087f219b819",
+    "url": "/static/js/main.c1e4cc07.chunk.js"
+  },
+  {
+    "revision": "41f96ad9b12a68fb853d",
+    "url": "/static/js/1.41f96ad9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c21794518c3e8e8a480a78473f597014",
+    "url": "/static/media/robot.c2179451.png"
+  },
+  {
+    "revision": "51d6269a95e38ddda57347eccb66b678",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.7babe1ef7953f67aaf3e14f2074209cd.js"
+  "/precache-manifest.f4ac8ab237f9836cb28e1589ca762faf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
